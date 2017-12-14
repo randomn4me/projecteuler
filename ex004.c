bd0cc4d774fed5186bdbb7481951da68da99ddd1
Philipp Jonczyk <p.jonczyk@posteo.de>
2017-12-14 14:06:48 +0100

    update

--- ex004.c
+++ ex004.c
@@ -19,15 +19,19 @@ int
 main(int argc, int **argv)
 {
 	int num = 0, largest = 0;
+	int a = 0, b = 0;
 	for (int i = 999; i > 99; i--) {
 		for (int j = 999; j > 99; j--) {
 			num = i * j;
 			if (is_palindrom(num) && num > largest) {
+				a = i;
+				b = j;
 				largest = num;
 			}
 		}
 	}
 	printf("Largest palindrom: %d\n", largest);
+	printf("Constructed by multiplying %d and %d\n", a, b);
 
 	return 0;
 }
---
+++ ex005.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int
+is_evenly_divisible(long num)
+{
+	for (long i = 11; i <= 20; ++i) {
+		if (num % i != 0)
+			return 0;
+	}
+	return 1;
+}
+
+int
+main(int argc, char **argv)
+{
+	int i;
+	for (i = 20;; i = i + 20) {
+		if (is_evenly_divisible(i)) {
+			break;
+		}
+	}
+	printf("%li\n", i);
+
+	return 0;
+}
